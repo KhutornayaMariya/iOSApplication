68b45229272c37dcde7a84a6a744b32a5803a6f3
m.khutornaya <m.khutornaya@vk.team>
2022-10-29 18:08:20 +0300

    fix firebase configuration

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
@@ -22,6 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.makeKeyAndVisible()
         let mainCoordinator = MainCoordinator()
         window.rootViewController = mainCoordinator.startApplication()
+        FirebaseApp.configure()
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -29,6 +32,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Произошла ошибка при разлогине")
+            return
+        }
     }
     
     func sceneDidBecomeActive(_ scene: UIScene) {
