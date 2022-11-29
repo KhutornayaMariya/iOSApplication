a1f7f939a58d927764a3cb7553a160c059734b19
m.khutornaya <m.khutornaya@vk.team>
2022-11-29 14:43:47 +0300

    add map

--- Navigation/Flow/PostViewController/PostViewController.swift
+++ Navigation/Flow/PostViewController/PostViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class PostViewController: UIViewController {
+
+    private var locationManager: CLLocationManager!
+    private var routePolyline: MKOverlay!
+
+    private lazy var mapView: MKMapView = {
+        let view = MKMapView()
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.delegate = self
+        if #available(iOS 16.0, *) {
+            view.preferredConfiguration = MKHybridMapConfiguration(elevationStyle: .flat)
+        } else {
+            // Fallback on earlier versions
+        }
+
+        return view
+    }()
     
-    var backgroundColor: UIColor = .white
-    
-    init(_ color: UIColor) {
-        super.init(nibName: nil, bundle: nil)
-        backgroundColor = color
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+        setUpLocationManager()
+        setUpPins()
+    }
+
+    private func setUp() {
+        view.backgroundColor = .white
+        setUpNavigationBar()
+
+        view.addSubview(mapView)
+
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    private func setUpNavigationBar() {
         let infoButtonItem = UIBarButtonItem(
             image: UIImage(systemName: "info"),
             style: UIBarButtonItem.Style.plain,
@@ -22,23 +58,110 @@ class PostViewController: UIViewController {
         )
         navigationItem.rightBarButtonItem = infoButtonItem
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+
+    private func setUpLocationManager() {
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager = locationManager
     }
-    
-    override func loadView() {
-        view = UIView()
-        view.backgroundColor = backgroundColor
-        self.title = "New Post!"
+
+    private func setUpPins() {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 55.73, longitude: 37.61)
+        annotation.title = "My pin"
+        mapView.addAnnotation(annotation)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+
+    private func addDirections(coordinate: CLLocationCoordinate2D) {
+        if let routePolyline = routePolyline {
+            self.mapView.removeOverlay(routePolyline)
+        }
+
+        let request = MKDirections.Request()
+        request.transportType = .automobile
+
+        let sourcePlacemark = MKPlacemark(coordinate: coordinate)
+        request.source = MKMapItem(placemark: sourcePlacemark)
+        print(mapView.userLocation.coordinate)
+
+        let destinationPlacemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 59.9, longitude: 30.3))
+        request.destination = MKMapItem(placemark: destinationPlacemark)
+
+        let direction = MKDirections(request: request)
+
+        direction.calculate { [weak self] response, error in
+            guard let self = self else { return }
+
+            guard let response = response else {
+                if let error = error {
+                    print(error)
+                }
+                return
+            }
+
+            guard let route = response.routes.first else { return }
+
+            self.routePolyline = route.polyline
+            self.mapView.addOverlay(self.routePolyline, level: .aboveRoads)
+
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+        }
     }
     
     @objc
-    func openInfoButtomSheet() {
+    private func openInfoButtomSheet() {
         present(InfoViewController(), animated: true, completion: nil)
+    }
+}
+
+extension PostViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            print("notDetermined")
+            manager.requestWhenInUseAuthorization()
+
+        case .restricted:
+            print("restricted")
+            break
+
+        case .denied:
+            print("deined")
+            break
+
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("authorized")
+            manager.requestLocation()
+            mapView.showsUserLocation = true
+            mapView.showsCompass = true
+            mapView.showsScale = true
+            manager.startUpdatingLocation()
+
+        @unknown default:
+            fatalError()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
+        addDirections(coordinate: location.coordinate)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
+
+extension PostViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let render = MKPolylineRenderer(overlay: overlay)
+        render.strokeColor = .systemBlue
+        render.lineWidth = 4.0
+        return render
     }
 }
